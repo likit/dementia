59968bfbfb007381d8b8e13a1f4ebef0d5addd12
Likit Preeyanon <preeyano@msu.edu>
2015-04-21 16:59:43 +0700

    added dbinit command
    added password field and hashing

--- webapp/manage.py
+++ webapp/manage.py
@@ -4,6 +4,7 @@ import os
 from app import create_app, db
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+from werkzeug.security import generate_password_hash
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -20,6 +21,19 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+@manager.command
+def dbinit():
+    """Init the database."""
+    db.drop_collection('users')
+    password = generate_password_hash('hardtoguess')
+    user_doc = {
+            'username': 'admin',
+            'email': 'likit.pre@mahidol.edu',
+            'password': password,
+            'role': 'admin',
+            'zone': 0,
+            }
+    db.users.insert(user_doc, safe=True)
 
 if __name__ == '__main__':
     manager.run()
