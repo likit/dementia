e1ce03d0a6777be4b474312c0cd5540ec35e9f94
Likit Preeyanon <preeyano@msu.edu>
2015-04-28 11:29:40 +0700

    resolved import conflicts for flask-admin

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -2,18 +2,19 @@ from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
+import pymongo
+
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask.ext.login import LoginManager
 from config import config
-import pymongo
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
-admin = Admin()
 conn = pymongo.Connection()
 db = conn['data-dev']
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -27,10 +28,17 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     login_manager.init_app(app)
-    admin.init_app(app)
 
-    from main.views import UserView
+    ''' Views has to be imported after app instance has been created,
+        otherwise relative import will not work.'''
+
+    from adminpage.views import UserView, MyAdminIndexView
+    admin = Admin(name='My Admin', index_view=MyAdminIndexView(),
+            base_template='admin/index.html')
+
     admin.add_view(UserView(db.users, 'User'))
+
+    admin.init_app(app)
 
     from main import main as main_blueprint
     from .auth import auth as auth_blueprint
