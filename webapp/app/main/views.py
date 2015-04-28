cbe843f505cced95eda72376973249241477a2f9
Likit Preeyanon <preeyano@msu.edu>
2015-04-28 09:44:42 +0700

    moved UserForm and UserView to admin folder

--- webapp/app/main/views.py
+++ webapp/app/main/views.py
@@ -1,36 +1,9 @@
 from flask import render_template, session, redirect, url_for
-from datetime import datetime
 from . import main
-from .forms import NameForm
 from .. import db
 
 from bson.objectid import ObjectId
-import flask_admin as admin
-from wtforms import form, fields
-
-from flask_admin.form import Select2Widget
-from flask_admin.contrib.pymongo import ModelView, filters
-from flask_admin.model.fields import InlineFormField, InlineFieldList
-
 from flask.ext.login import login_required, current_user
-
-# User admin
-class UserForm(form.Form):
-    username = fields.TextField('Username')
-    email = fields.TextField('Email')
-    role = fields.TextField('Role')
-    password = fields.PasswordField('Password')
-
-
-class UserView(ModelView):
-    column_list = ('username', 'email', 'role')
-    column_sortable_list = ('username', 'email', 'password')
-
-    form = UserForm
-
-    def is_accessible(self):
-        # return current_user.is_authenticated()
-        return True
 
 @main.route('/', methods=['GET', 'POST'])
 @login_required
