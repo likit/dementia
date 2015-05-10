b5c00c3c1642612c1c9f40b64b1a313f174a2d1a
Likit Preeyanon <preeyano@msu.edu>
2015-05-10 17:05:29 +0700

    edited sql to join three tables

--- retrieve_district_amphur_province_tsv.sql
+++ retrieve_district_amphur_province_tsv.sql
@@ -1,2 +1,5 @@
 use Provinces;
-select DISTRICT_NAME, AMPHUR_NAME from district inner join amphur using (AMPHUR_ID);
+-- select DISTRICT_NAME, AMPHUR_NAME from district inner join amphur using (AMPHUR_ID);
+select DISTRICT_NAME, AMPHUR_NAME, PROVINCE_NAME from district
+    inner join amphur on district.AMPHUR_ID = amphur.AMPHUR_ID
+    join province on district.PROVINCE_ID = province.PROVINCE_ID;
