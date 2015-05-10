be46c16e56013c08bdb4953a10f0f1b5a7310947
Likit Preeyanon <preeyano@msu.edu>
2015-05-10 19:21:52 +0700

    added more fields and replaced email with pid.

--- webapp/app/auth/forms.py
+++ webapp/app/auth/forms.py
@@ -1,36 +1,54 @@
+# -*- coding: UTF-8 -*-
+
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import Required, Email, Length, Regexp, EqualTo
+from wtforms import (StringField, PasswordField,
+                        BooleanField, SubmitField, IntegerField,
+                        SelectField, TextAreaField)
+from wtforms.validators import (Required, Email, Length,
+                                    Regexp, EqualTo, Optional)
+from wtforms.widgets import Select
 from wtforms import ValidationError
 from .. import db
 
+import sys
+
 class LoginForm(Form):
-    username = StringField('Username',
-            validators=[Required(), Length(1, 64)])
+    pid = StringField('Username',
+            validators=[Required(), Length(1, 13,
+                message='PID must be 13 digits long')])
     password = PasswordField('Password',
             validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
 
 class RegistrationForm(Form):
-    email = StringField('Email', validators=[Required(),
-        Length(1,64), Email()])
-    username = StringField('Account ID', validators=[
-        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-            'Usernames must have only letters, '
-            'numbers, dots or underscores')])
-    name = StringField('Name', validators=[Required(), Length(1,200)])
-    lastname = StringField('Lastname', validators=[Required(), Length(1,200)])
-    org = StringField('Organization', validators=[Required(), Length(1,200)])
-    zone = IntegerField('Zone', validators=[Required()])
-    password = PasswordField('Password', validators=[
-        Required(), EqualTo('password2', message='Passwords must match.')])
-    password2 = PasswordField('Confirm password', validators=[Required()])
-    submit = SubmitField('Register')
+    pid = IntegerField(u'รหัสบัตรประชาชน', validators=[Required(),
+        Length(1, 13, message=u'รหัสบัตรประชาชนต้องมี 13 หลัก')])
+    password = PasswordField(u'รหัสผ่าน', validators=[
+        Required(), EqualTo('password2', message=u'รหัสผ่านต้องตรงกัน')])
+    password2 = PasswordField(u'รหัสผ่านอีกครั้ง', validators=[Required()])
+    title = SelectField(u'คำนำหน้า', choices=[(u'นาย',u'นาย'), (u'นาง',u'นาง'),
+                            (u'น.ส.', u'น.ส.')], option_widget=Select,
+                            validators=[Required()])
+    name = StringField(u'ชื่อ', validators=[Required()])
+    lastname = StringField(u'นามสกุล', validators=[Required()])
+    position = StringField(u'ตำแหน่ง', validators=[Required()])
+    phone = StringField(u'โทรศัพท์', validators=[Required()])
+    cell = StringField(u'มือถือ', validators=[Optional()])
 
-    def validate_email(self, field):
-        if db.users.find_one({'email': field.data}):
-            raise ValidationError('Email already registered.')
+    org = StringField(u'องค์กร', validators=[Required()])
+    org_address = StringField(u'เลขที่', validators=[Required()])
+    mhoo = StringField(u'หมู่', validators=[Required()])
+    province = SelectField(u'จังหวัด', validators=[Required()])
+    district = SelectField(u'อำเภอ', validators=[Required()])
+    tambon = SelectField(u'ตำบล', validators=[Required()])
+    objective = TextAreaField(u'วัตถุประสงค์')
+
+    submit = SubmitField(u'ลงทะเบียน')
+
+    # def validate_email(self, field):
+    #     if db.users.find_one({'email': field.data}):
+    #         raise ValidationError('Email already registered.')
 
     def validate_username(self, field):
         if db.users.find_one({'username': field.data}):
