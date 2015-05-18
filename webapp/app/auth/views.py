b8ec968252ab01081a2362b770d3104c5a810416
Likit Preeyanon <preeyano@msu.edu>
2015-05-18 10:44:04 +0700

    remove confirmation token
    email is sent to admin email from config file
    
    admin have to log in to verify accounts

--- webapp/app/auth/views.py
+++ webapp/app/auth/views.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import copy
 
 from collections import defaultdict
@@ -11,7 +12,7 @@ from .. import db
 from datetime import datetime
 from flask.ext.login import login_user, login_required, logout_user
 from .forms import LoginForm, RegistrationForm
-from ..models import User, generate_confirmation_token
+from ..models import User
 from ..email import send_email
 from werkzeug.security import check_password_hash, generate_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -65,7 +66,6 @@ def register():
     form.tambon.choices = [('', '')] + [(x,x) for x in tambon_list]
 
     if form.validate_on_submit():
-        token = generate_confirmation_token(form.pid.data)
         user_doc = {
                 'pid': form.pid.data,
                 'password': generate_password_hash(form.password.data),
@@ -88,13 +88,16 @@ def register():
                 'verified': False,
                 'create_date_time': datetime.today(),
                 'role': 'staff',
-                'verify_token': token,
                 }
+
         db.users.insert(user_doc, safe=True)
-        send_email(to='donsanova@gmail.com',
-                subject='%s %s' % (form.name.data, form.lastname.data),
-                template='auth/email/confirm', user=user_doc, token=token)
-        flash('Your account verification is now pending.')
+        send_email(to=current_app.config['ADMIN_EMAIL'],
+                subject=u' New Account verification: ชื่อผู้ขอใช้ %s %s' % \
+                        (form.name.data, form.lastname.data),
+                template='auth/email/confirm', user=user_doc)
+
+        flash(u'ระบบได้ส่งข้อมูลของท่านไปรอการตรวจสอบจากเจ้าหน้าที่แล้ว\n'
+                u'ท่านสามารถทำการลงชื่อเข้าใช้เพื่อตรวจสอบสถานะได้ตลอดเวลา')
         return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html', form=form,
@@ -102,28 +105,6 @@ def register():
                                 amphurs=json_util.dumps(amphurs),
                                 tambons=json_util.dumps(tambons))
 
-@auth.route('/confirm/<token>')
-# @login_required
-def confirm(token):
-    s = Serializer(current_app.config['SECRET_KEY'])
-    user = db.users.find_one({'verify_token': token})
-    if not user:
-        flash('Your account cannot be verified.')
-        return redirect(url_for('main.index'))
-
-    try:
-        data = s.loads(token)
-    except:
-        return False
-    if data.get('confirm') != user['pid']:
-        flash('Your account remains unverified.')
-        return redirect(url_for('main.index'))
-    else:
-        new_user = copy.deepcopy(user)
-        new_user['verified'] = True
-        db.users.update({'verify_token': token}, new_user, safe=True)
-        flash('Your account has been verified.')
-        return redirect(url_for('main.index'))
 
 # @auth.route('/confirm')
 # @login_required
