555dc693a85b07a53b077987994e3fa751a9cd79
Likit Preeyanon <preeyano@msu.edu>
2015-05-18 10:45:10 +0700

    changed email subject prefix

--- webapp/config.py
+++ webapp/config.py
@@ -4,8 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'center_of_data_mining'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SUBJECT_PREFIX = '[Confirmation]'
+    MAIL_SUBJECT_PREFIX = '[TEIS]'
     MAIL_SENDER = 'Likit Preeyanon <likit.pre@mahidol.edu>'
+    ADMIN_EMAIL = 'donsanova@gmail.com'
     # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
