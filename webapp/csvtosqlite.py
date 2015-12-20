e4a3363de740d9defa1801760c050eb1be5a3089
Likit Preeyanon <preeyano@msu.edu>
2015-12-20 15:31:06 +0700

    added insert question and answer health tables

--- webapp/csvtosqlite.py
+++ webapp/csvtosqlite.py
@@ -43,6 +43,31 @@ class Answer(db.Model):
     question_id = db.Column(db.Integer,
                         db.ForeignKey('eh_questions.id'))
 
+    def __repr__(self):
+        return '<Answer %d>' % self.id
+
+
+class QuestionHealth(db.Model):
+    __tablename__=  'eh_question_healths'
+    id = db.Column(db.Integer, primary_key=True)
+    desc = db.Column(db.String(255))
+    parent_qh_id_ = db.Column(db.Integer)
+    answers = db.relationship('AnswerHealth',
+            backref='question')
+
+
+class AnswerHealth(db.Model):
+    __tablename__ = 'eh_answer_healths' # table name is maintaiend for reference.
+    id = db.Column(Integer, primary_key=True)
+    seq_ = db.Column(Integer)
+    desc = db.Column(String(255))
+    question_id = db.Column(db.Integer,
+            db.ForeignKey('eh_question_healths.id'))
+    other_ = db.Column(db.Boolean)
+    disease_ = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Answer %d>' % self.id
 
 def insert_question_type(infile):
     reader = csv.reader(open(infile))
@@ -86,6 +111,43 @@ def insert_answer(infile):
         n += 1
         if n == 5: break
 
+
+def to_bool(str_value):
+    keys = {'N': False, 'Y': True}
+    return keys[str_value]
+
+
+def insert_question_health(infile):
+    reader = csv.reader(open(infile))
+    n = 0
+    for row in reader:
+        ans = QuestionHealth(
+                id=row[0],
+                desc=unicode(row[1], 'utf8'),
+                parent_qh_id_=row[2]
+                )
+        db.session.add(ans)
+        db.session.commit()
+
+
+def insert_answer_health(infile):
+    reader = csv.reader(open(infile))
+    n = 0
+    for row in reader:
+        ans = AnswerHealth(
+                id=row[0],
+                seq_=row[1],
+                desc=unicode(row[2], 'utf8'),
+                question_id=row[3],
+                other_=to_bool(unicode(row[4], 'utf8')),
+                disease_=to_bool(unicode(row[5], 'utf8')),
+                )
+        db.session.add(ans)
+        db.session.commit()
+        n += 1
+        if n == 5: break
+
+
 def main():
     try:
         db.create_all()
@@ -97,8 +159,17 @@ def main():
         # insert_question_type(os.path.join(datadir,
         #                                 'eh_question_type.csv'))
 
-        insert_question(os.path.join(datadir, 'eh_question.csv'))
-        insert_answer(os.path.join(datadir, 'eh_answer.csv'))
+        # insert_question(os.path.join(datadir, 'eh_question.csv'))
+        # insert_answer(os.path.join(datadir, 'eh_answer.csv'))
+        insert_answer_health(os.path.join(datadir, 'eh_answer_health.csv'))
+        # insert_question_health(os.path.join(datadir, 'eh_question_health.csv'))
+
+        # for row in Question.query.all():
+        #     print("{0} {1}".format(row.id, row.qname.encode('utf8')))
+
+        # for row in Answer.query.get_by(question_id=1):
+        #     print("{0} {1} {2}".format(row.id, row.desc.encode('utf8'),
+        #             row.question.qname.encode('utf8')))
 
 
 if __name__=='__main__':
